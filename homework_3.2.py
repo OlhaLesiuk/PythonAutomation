848af8ec522b3489b1f7aa6d36565d5f42971c4a
OlhaLesiuk <vazhyk@gmail.com>
2022-08-27 21:24:32 +0300

    Fixed issues in homework_3.2, 3.5

--- homework_3.2.py
+++ homework_3.2.py
@@ -1,4 +1,4 @@
-Eleks = {'John', 'Mark', 'Jane', 'Robert', 'Kim', 'Jessica', 'Rachel'}
-Toshiba = {'Monica', 'Jane', 'Kim', 'Steven', 'Bob', 'Susan', 'Brit'}
-Toshiba_feat_Eleks = Eleks.union(Toshiba)
-print(Toshiba_feat_Eleks)
+Eleks = ['John', 'Mark', 'Jane', 'Robert', 'Kim', 'Jessica', 'Rachel']
+Toshiba = ['Monica', 'Jane', 'Kim', 'Steven', 'Bob', 'Susan', 'Brit']
+Toshiba.extend(Eleks)
+print(Toshiba, Eleks)
