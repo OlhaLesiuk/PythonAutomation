2c5540e776f7cbd3007ec0b735e477a9016fef58
OlhaLesiuk <vazhyk@gmail.com>
2022-08-28 11:10:12 +0300

    Fixed homework_3.5.py
    Changed VIP_box to tuple

--- homework_3.5.py
+++ homework_3.5.py
@@ -1,11 +1,13 @@
-VIP_box = frozenset({1: 'the seat is taken', 2: 'the seat is taken', 3: 'the seat is taken', 4: 'the seat is taken',
-           5: 'the seat is taken', 6: 'the seat is taken', 7: 'the seat is taken', 8: 'the seat is taken',
-           9: 'the seat is taken', 10: 'the seat is taken'})
+VIP_box = ((1, 'the seat is taken'), (2, 'the seat is taken'), (3, 'the seat is taken'),
+           (4, 'the seat is taken',), (5, 'the seat is taken'), (6, 'the seat is taken'),
+           (7, 'the seat is taken'), (8, 'the seat is taken'),
+           (9, 'the seat is taken'), (10, 'the seat is taken'))
 
 common_room = {1: 'the seat is taken', 2: 'the place is free', 3: 'the place is free', 4: 'the seat is taken',
                5: 'the place is free', 6: 'the seat is taken', 7: 'the seat is taken', 8: 'the place is free',
                9: 'the place is free', 10: 'the seat is taken', 11: 'the place is free', 12: 'the seat is taken'}
 
+print('the place is free in the VIP_box?', VIP_box)
+
 free_places_common = common_room.get(2, 'the place is free')
 print('the place is free in the common rooms?', free_places_common)
-
