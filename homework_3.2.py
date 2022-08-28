39d2b05a6b34c97f3183c21024f4217a4560fe13
OlhaLesiuk <vazhyk@gmail.com>
2022-08-28 21:04:36 +0300

    Added .clear method to Homework_3.2.py

--- homework_3.2.py
+++ homework_3.2.py
@@ -1,4 +1,5 @@
 Eleks = ['John', 'Mark', 'Jane', 'Robert', 'Kim', 'Jessica', 'Rachel']
 Toshiba = ['Monica', 'Jane', 'Kim', 'Steven', 'Bob', 'Susan', 'Brit']
 Toshiba.extend(Eleks)
-print(Toshiba, Eleks)
+Eleks.clear()
+print(Toshiba)
