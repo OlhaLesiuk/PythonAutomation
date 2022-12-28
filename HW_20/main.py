425bd49709ca2e740342704e5d503a39618410ce
OlhaLesiuk <vazhyk@gmail.com>
2022-12-28 22:24:17 +0200

    Fix from 28.12.2022

--- HW_20/main.py
+++ HW_20/main.py
@@ -4,7 +4,7 @@ getresponse = get('https://reqres.in/api/users?page=2')
 postresponse = post('https://reqres.in/api/users')
 putresponse = put('https://reqres.in/api/users/2')
 patchresponse = patch('https://reqres.in/api/users/2')
-deleteresponse = delete('https://reqres.in')
+deleteresponse = delete('https://reqres.in/api/users/2')
 print(getresponse.content, postresponse.content, putresponse.content, patchresponse.content, deleteresponse.content)
 print(getresponse.text, postresponse.text, putresponse.text, patchresponse.text, deleteresponse.text)
 print(getresponse.status_code, postresponse.status_code, putresponse.status_code, patchresponse.status_code,
--- HW_20/test_api.py
+++ HW_20/test_api.py
@@ -5,9 +5,36 @@ import json
 
 
 def test_get_user_200():
-    response = UserAPI().get_user(7)
+    getresponse = UserAPI().get_user(7)
+    assert getresponse.status_code == HTTPStatus.OK, f'Status code is not as expected\nActual ' \
+                                                     f'{getresponse.status_code}' \
+                                                     f'\nExpected: {HTTPStatus.OK}'
+
+
+def test_post_user_201():
+    postresponse = UserAPI().post_user('morpheus', 'leader')
+    assert postresponse.status_code == HTTPStatus.CREATED, f'Status code is not as expected\nActual ' \
+                                                           f'{postresponse.status_code}' f'\nExpected: ' \
+                                                           f'{HTTPStatus.CREATED}'
+
+
+def test_put_user_200():
+    response = UserAPI().put_user('morpheus', 'zion resident')
     assert response.status_code == HTTPStatus.OK, f'Status code is not as expected\nActual {response.status_code}' \
                                                   f'\nExpected: {HTTPStatus.OK}'
+
+
+def test_patch_user_200():
+    patchresponse = UserAPI().patch_user('morpheus', 'zion resident')
+    assert patchresponse.status_code == HTTPStatus.OK, f'Status code is not as expected\nActual ' \
+                                                       f'{patchresponse.status_code}' f'\nExpected: {HTTPStatus.OK}'
+
+
+def test_delete_user_204():
+    deleteresponse = UserAPI().delete_user(7)
+    assert deleteresponse.status_code == HTTPStatus.NO_CONTENT, f'Status code is not as expected\nActual ' \
+                                                                f'{deleteresponse.status_code}' \
+                                                                f'\nExpected: {HTTPStatus.NO_CONTENT}'
 
 
 def test_user_data(create_user):
